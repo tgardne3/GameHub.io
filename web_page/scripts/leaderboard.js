85e794d8d255d830cf33e5eb5b8f5329a3b302e9
Trey Gardner <tgardne3@uccs.edu>
2026-03-03 14:01:10 -0700

    working on leaderboard UI

---
+++ web_page/scripts/leaderboard.js
@@ -0,0 +1,11 @@
+//name: Azam, Brandon, David, Trey
+//class: CS3300
+
+//display leaderboard info
+function display_leaderboard() {
+
+    const container = Document.GetElementById("leaderboard-container");
+    container.innerHTML = '';
+    
+
+}//display leaderboard
