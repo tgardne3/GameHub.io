ad6a5e1bff028e4f6af415e739067a820a72ddaf
Oxygen1282 <dzarkovacki03@gmail.com>
2026-03-12 10:19:47 -0600

    added some comments to snake_game.js

--- web_page/games/snake/snake_game.js
+++ web_page/games/snake/snake_game.js
@@ -1,24 +1,24 @@
 // Declare variables
 let end_game = false;
 
-let fruit = {x: 8, y: 6};
+let fruit = {x: 8, y: 6}; // initial fruit spawn
 
-let snake = [
+let snake = [       // initial snake
     {x: 5, y: 6},
     {x: 4, y: 6},
     {x: 3, y: 6}
 ];
 
-let score = 3;
+let score = 3;      // initial score
 
-let boundaries = new Set([
+let boundaries = new Set([  // set for boundry collision 
     "0,0", "1,0", "2,0", "3,0", "4,0", "5,0", "6,0", "7,0", "8,0", "9,0", "10,0", "11,0", // top wall
     "0,1", "0,2", "0,3", "0,4", "0,5", "0,6", "0,7", "0,8", "0,9", "0,10",  // left wall
     "11,1", "11,2", "11,3", "11,4", "11,5", "11,6", "11,7", "11,8", "11,9", "11,10", // right wall
     "0,11", "1,11", "2,11", "3,11", "4,11", "5,11", "6,11", "7,11", "8,11", "9,11", "10,11", "11,11", // bottom wall
 ])
 
-let snake_set = new Set([
+let snake_set = new Set([   // set for snake collision
     "5,6",
     "4,6",
     "3,6",
@@ -45,11 +45,11 @@ function next_head(direction) {
 }
 // Function to move the snake
 function move_snake(direction) {
-    if (direction != "s") {
-        const new_head = next_head(direction);
-        const new_set_key = `${new_head.x},${new_head.y}`;
+    if (direction != "s") {  // keeps snake stationary at start of game until direction key is pressed
+        const new_head = next_head(direction);  // add next square to snake array
+        const new_set_key = `${new_head.x},${new_head.y}`;  // add next square to set
 
-        detect_collision(new_set_key);
+        detect_collision(new_set_key);  // check collision
 
         // add new head
         snake.unshift(new_head);
@@ -74,19 +74,19 @@ function grow_snake(direction) {
     snake.unshift(new_head);
     snake_set.add(new_set_key);
 
-    fruit = spawn_fruit(snake_set);
+    fruit = spawn_fruit(snake_set); // spawn new fruit
 
     score++;
     update_score_display();
 }
 
 // Function to detect collisions
-function detect_collision(head) {
-    if (snake_set.has(head)) {
+function detect_collision(head) {  
+    if (snake_set.has(head)) {  // detect a colision with the snake
         end_game = true;
         return;
     }
-    if (boundaries.has(head)) {
+    if (boundaries.has(head)) { // detect a colission with the walls
         end_game = true;
         return;
     }
@@ -101,38 +101,38 @@ function is_game_over() {
 // Function to spawn fruit in random location
 function spawn_fruit(snake_set) {
 
-    let rand_x = Math.floor((Math.random() * 10) + 1);
+    let rand_x = Math.floor((Math.random() * 10) + 1);  // random value from 1-11
     let rand_y = Math.floor((Math.random() * 10) + 1);
-    let rand_set_key = `${rand_x},${rand_y}`;
+    let rand_set_key = `${rand_x},${rand_y}`;       // put in in set format to easily check for snake values
 
-    while (snake_set.has(rand_set_key) || boundaries.has(rand_set_key)) {
+    while (snake_set.has(rand_set_key) || boundaries.has(rand_set_key)) { // check fruit is not in snake
         rand_x = Math.floor((Math.random() * 10) + 1);
         rand_y = Math.floor((Math.random() * 10) + 1);
 
         rand_set_key = `${rand_x},${rand_y}`;
     }
 
-    return {x: rand_x, y: rand_y};
+    return {x: rand_x, y: rand_y};  // return fruit corrdinates if not in snake
 }
 
 // Function to reset the game
 function reset_game() {
     end_game = false;
 
-    fruit = {x: 8, y: 6};
+    fruit = {x: 8, y: 6};  // set fruit to default spot
 
-    snake = [
+    snake = [               // reset snake 
     {x: 5, y: 6},
     {x: 4, y: 6},
     {x: 3, y: 6}
     ];
 
-    snake_set = new Set([
+    snake_set = new Set([   // reset internal snake values
     "5,6",
     "4,6",
     "3,6",
     ])
-    
+
     current_direction = "s";
 
     score = 3;
