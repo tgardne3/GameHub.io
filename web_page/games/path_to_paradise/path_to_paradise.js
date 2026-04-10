476b641a68156ac70d9116d0c9a5ec199e884eb2
Trey <tgardne3@uccs.edu>
2026-04-09 22:51:04 -0600

    path to paradise game outline and intro done!

--- web_page/games/path_to_paradise/path_to_paradise.js
+++ web_page/games/path_to_paradise/path_to_paradise.js
@@ -1,38 +1,91 @@
 //name: Azam, Brandon, David, Trey
 //class: CS3300
 
-let story;
+let default_choice = 0.0;
+let noChoice = false;
+let a_choice = 0.1;
+let b_choice = 0.2;
+let c_choice = 0.3;
+let d_choice = 0.4;
+let story_set = 0;
+let current_set = (0.0).toFixed(1);
+let story = {};
 
-async function load_story() {
-    try {
-        const response = await fetch("./story.json");
-        story = response.json();
-        console.log(story);
-    } catch (error) {
-        console.error('Failed to load story.json', error);
+
+//path to paradise game logic
+function display_text() {
+    //set story key
+    let key = current_set.toString();
+    console.log(`Key: ${key}`);
+
+    //grab body from HTML
+    const text_body = document.getElementById("text-body");
+    let text_string = story[key]["text"];
+    for (let i = 0; i <= text_string.length; i++) {
+        //if HTML tag
+        if (text_string[i] === "<") {
+            i = text_string.indexOf(">", i);
+            continue;
+        }
+
+        setTimeout(() => {
+            text_body.innerHTML = text_string.slice(0, i);
+        }, 35 * i);
+    }
+
+    //check if custom answers exist
+    if(story[key]["a"]) {
+        btn_a.innerHTML = story[key]["a"];
+        btn_b.innerHTML = story[key]["b"];
+        btn_c.innerHTML = story[key]["c"];
+        btn_d.innerHTML = story[key]["d"];
+    } else {//default answers
+        btn_a.innerHTML = `<b>A:</b> Continue...`;
+        btn_b.innerHTML = `<b>B:</b> Continue...`;
+        btn_c.innerHTML = `<b>C:</b> Continue...`;
+        btn_d.innerHTML = `<b>D:</b> Continue...`;
+
+        noChoice = true;
     }
 }
 
 
 
-//path to paradise game logic
-function display_text() {
-
+//loading the text data in story.json
+async function load_story() {
+    try {
+        const response = await fetch("./story.json");
+        story = await response.json();
+        console.log(story);
+        display_text();
+    } catch (error) {
+        console.error('Failed to load story.json', error);
+    }
 }
-
+//logic to changing text based on user response
 function update_choice(choice) {
-    console.log(choice);
+    if(noChoice === false) {
+        current_set = (story_set + choice);
+    } else {
+        current_set = story_set.toFixed(1);
+        noChoice = false;
+    }
+    display_text(choice);
+    story_set++;
 }
+
+
+/* app initialization */
+load_story();
 
 /* Document Objects */
 const btn_a = document.getElementById("btn-a");
-if(btn_a) {
-    load_story();
-    btn_a.addEventListener("click", update_choice(0.1));
-    const btn_b = document.getElementById("btn-b");
-    btn_a.addEventListener("click", update_choice(0.2));
-    const btn_c = document.getElementById("btn-c");
-    btn_a.addEventListener("click", update_choice(0.3));
-    const btn_d = document.getElementById("btn-d");
-    btn_a.addEventListener("click", update_choice(0.4));
-}
+const btn_b = document.getElementById("btn-b");
+const btn_c = document.getElementById("btn-c");
+const btn_d = document.getElementById("btn-d");
+
+btn_a.addEventListener("click", () => update_choice(a_choice));
+btn_b.addEventListener("click", () => update_choice(b_choice));
+btn_c.addEventListener("click", () => update_choice(c_choice));
+btn_d.addEventListener("click", () => update_choice(d_choice));
+
